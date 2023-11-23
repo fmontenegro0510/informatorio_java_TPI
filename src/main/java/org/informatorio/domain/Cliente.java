082df6e29cd32e180eed478060b4b04021f63a7a
Francisco Montenegro <franciscomontenegro22@gmail.com>
2023-11-23 19:50:12 -0300

    Work in cliente Object

--- src/main/java/org/informatorio/domain/Cliente.java
+++ src/main/java/org/informatorio/domain/Cliente.java
@@ -1,4 +1,37 @@
 package org.informatorio.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
+    private Long idCliente;
+    private String nombre;
+    private String direccion;
+    private List<Cuenta> cuentas = new ArrayList<>();
+
+    public Cliente(Long idCliente, String nombre, String direccion) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public List<Cuenta> getCuentas() {
+        return cuentas;
+    }
+
+    public void agregarCuenta(Cuenta cuenta) {
+        cuentas.add(cuenta);
+    }
 }
