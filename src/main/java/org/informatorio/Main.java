2d269070712a14ae135fe0a94517da1fb0d8b585
Francisco Montenegro <franciscomontenegro22@gmail.com>
2023-11-23 21:01:02 -0300

    Work in menu

--- src/main/java/org/informatorio/Main.java
+++ src/main/java/org/informatorio/Main.java
@@ -1,11 +1,151 @@
 package org.informatorio;
 
+import org.informatorio.domain.Banco;
+import org.informatorio.domain.Cliente;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("A POR ELLO PERRITO!");
+        Banco banco = new Banco();
 
+        while (true) {
+            // Mostrar opciones del menú
+            System.out.println("----- Menú Principal -----");
+            System.out.println("1. Crear nuevo cliente");
+            System.out.println("2. Mostrar todos los clientes");
+            System.out.println("3. Abrir nueva cuenta para un cliente");
+            System.out.println("4. Consultar saldo de una cuenta");
+            System.out.println("5. Calcular intereses de una cuenta de ahorro");
+            System.out.println("6. Exportar reporte de cuentas a CSV");
+            System.out.println("7. Salir");
+            System.out.print("Seleccione una opción: ");
 
+            // Leer la opción del usuario
+            int opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    // Crear nuevo cliente
+                    System.out.print("Ingrese el nombre del cliente: ");
+                    String nombre = scanner.next();
+                    System.out.print("Ingrese la dirección del cliente: ");
+                    String direccion = scanner.next();
+
+                    // Crear cliente y agregarlo al banco
+                    Cliente nuevoCliente = banco.crearCliente(nombre, direccion);
+                    System.out.println("Cliente creado con éxito.");
+                    break;
+
+                case 2:
+                    // Mostrar todos los clientes
+                    List<Cliente> clientes = banco.obtenerTodosLosClientes();
+
+                    if (clientes.isEmpty()) {
+                        System.out.println("No hay clientes registrados.");
+                    } else {
+                        System.out.println("----- Lista de Clientes -----");
+                        for (Cliente cliente : clientes) {
+                            System.out.println("ID: " + cliente.getIdCliente() + ", Nombre: " + cliente.getNombre() + ", Dirección: " + cliente.getDireccion());
+                            System.out.println("Cuentas:");
+
+                            for (Cuenta cuenta : cliente.getCuentas()) {
+                                System.out.println("   - Número de Cuenta: " + cuenta.getNumeroCuenta() + ", Saldo: " + cuenta.getSaldo());
+                            }
+
+                            System.out.println("-----------------------------");
+                        }
+                    }
+                    break;
+
+                case 3:
+                    // Abrir nueva cuenta para un cliente
+                    System.out.print("Ingrese el ID del cliente: ");
+                    Long idClienteCuenta = scanner.nextLong();
+                    Cliente clienteParaCuenta = banco.buscarClientePorId(idClienteCuenta);
+
+                    if (clienteParaCuenta != null) {
+                        System.out.println("Seleccione el tipo de cuenta:");
+                        System.out.println("1. Cuenta de Ahorro");
+                        System.out.println("2. Cuenta Corriente");
+                        int tipoCuenta = scanner.nextInt();
+
+                        switch (tipoCuenta) {
+                            case 1:
+                                // Abrir cuenta de ahorro
+                                System.out.print("Ingrese el monto inicial: ");
+                                double montoInicialAhorro = scanner.nextDouble();
+                                banco.abrirCuenta(clienteParaCuenta, Banco.TipoCuenta.AHORRO, montoInicialAhorro);
+                                break;
+
+                            case 2:
+                                // Abrir cuenta corriente
+                                System.out.print("Ingrese el monto inicial: ");
+                                double montoInicialCorriente = scanner.nextDouble();
+                                banco.abrirCuenta(clienteParaCuenta, Banco.TipoCuenta.CORRIENTE, montoInicialCorriente);
+                                break;
+
+                            default:
+                                System.out.println("Opción no válida.");
+                        }
+                    } else {
+                        System.out.println("Cliente no encontrado.");
+                    }
+                    break;
+
+                case 4:
+                    // Consultar saldo de una cuenta
+                    System.out.print("Ingrese el ID del cliente: ");
+                    Long idClienteConsulta = scanner.nextLong();
+                    Cliente clienteConsulta = banco.buscarClientePorId(idClienteConsulta);
+
+                    if (clienteConsulta != null) {
+                        System.out.print("Ingrese el número de cuenta: ");
+                        String numeroCuentaConsulta = scanner.next();
+                        banco.consultarSaldo(clienteConsulta, numeroCuentaConsulta);
+                    } else {
+                        System.out.println("Cliente no encontrado.");
+                    }
+                    break;
+
+                case 5:
+                    // Calcular intereses de una cuenta de ahorro
+                    System.out.print("Ingrese el ID del cliente: ");
+                    Long idClienteIntereses = scanner.nextLong();
+                    Cliente clienteIntereses = banco.buscarClientePorId(idClienteIntereses);
+
+                    if (clienteIntereses != null) {
+                        System.out.print("Ingrese el número de cuenta de ahorro: ");
+                        String numeroCuentaAhorro = scanner.next();
+                        banco.calcularIntereses(clienteIntereses, numeroCuentaAhorro);
+                    } else {
+                        System.out.println("Cliente no encontrado.");
+                    }
+                    break;
+
+                case 6:
+                    // Exportar reporte de cuentas a CSV
+                    banco.exportarReporteCuentasCSV();
+                    break;
+
+                case 7:
+                    // Salir del programa
+                    System.out.println("Saliendo del programa.");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+            }
+        }
     }
-
 }
--- src/main/java/org/informatorio/domain/Banco.java
+++ src/main/java/org/informatorio/domain/Banco.java
@@ -2,12 +2,52 @@ package org.informatorio.domain;
 
 
 import com.opencsv.CSVWriter;
+import org.informatorio.enums.TipoCuenta;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Banco {
 
+    private List<Cliente> clientes;
+
+    public Banco() {
+        this.clientes = new ArrayList<>();
+    }
+
+    public void agregarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public void abrirCuenta(Cliente cliente, TipoCuenta tipoCuenta, double montoInicial) {
+        Cuenta nuevaCuenta;
+        if (tipoCuenta == TipoCuenta.AHORRO) {
+            nuevaCuenta = new CuentaAhorro(montoInicial);
+        } else {
+            nuevaCuenta = new CuentaCorriente(montoInicial);
+        }
+
+        cliente.agregarCuenta(nuevaCuenta);
+    }
+
+    public void depositar(Cuenta cuenta, double monto) {
+        cuenta.depositar(monto);
+    }
+
+    public void retirar(Cuenta cuenta, double monto) {
+        cuenta.retirar(monto);
+    }
+
+    public void consultarSaldo(Cuenta cuenta) {
+        double saldo = cuenta.getSaldo();
+        System.out.println("Saldo de la cuenta: " + saldo);
+    }
+
+    public void calcularIntereses(CuentaAhorro cuentaAhorro) {
+        cuentaAhorro.calcularIntereses();
+    }
 
     public void exportarReporteCuentasCSV() {
         String csvFile = "reporte_cuentas.csv";
@@ -16,7 +56,7 @@ public class Banco {
             // Escribir encabezados en el archivo CSV
             writer.writeNext(new String[]{"Número único del titular", "Nombre de titular", "Saldo", "Tipo"});
 
-            // Escribir detalles de las cuentas
+            // Escribir detalles de las cuentas en el archivo CSV
             for (Cliente cliente : clientes) {
                 for (Cuenta cuenta : cliente.getCuentas()) {
                     writer.writeNext(new String[]{cliente.getIdCliente().toString(), cliente.getNombre(), Double.toString(cuenta.getSaldo()), cuenta.getTipoCuenta().toString()});
